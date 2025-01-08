3ccd0757cd4e52b1e868456de7c9694071590ab2
nekflamm <kevin.empociello@epitech.eu>
2025-01-08 09:58:35 +0100

    Files cleaning

--- Src/display_kingmeter.c
+++ Src/display_kingmeter.c
@@ -187,7 +187,7 @@ void KingMeter_Init (KINGMETER_t* KM_ctx)
     KM_ctx->Rx.Throttle                     = KM_THROTTLE_ON;
     KM_ctx->Rx.CruiseControl                = KM_CRUISE_OFF;
     KM_ctx->Rx.OverSpeed                    = KM_OVERSPEED_NO;
-    KM_ctx->Rx.SPEEDMAX_Limit              = (uint16_t) (spd_max1 * 10);
+    KM_ctx->Rx.SPEEDMAX_Limit           = (uint16_t) (spd_max1 * 10);
     KM_ctx->Rx.CUR_Limit_mA                = 150;
 
     // Parameters to be send to display in operation mode:
